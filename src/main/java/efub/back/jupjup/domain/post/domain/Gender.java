f6983e44229a59a99fb814e9d5141afcea7e2ff4
chhaewxn <chaewon1019@ewhain.net>
2023-10-13 04:27:52 +0900

    chore: Post Enum 코드 수정

--- src/main/java/efub/back/jupjup/domain/post/domain/Gender.java
+++ src/main/java/efub/back/jupjup/domain/post/domain/Gender.java
@@ -1,7 +1,14 @@
 package efub.back.jupjup.domain.post.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Gender {
-	ANY, // 성별무관
-	MALE, // 남성만
-	FEMALE // 여성만
+	ANY("any"),
+	FEMALE("female"),
+	MALE("male");
+
+	private final String description;
 }
