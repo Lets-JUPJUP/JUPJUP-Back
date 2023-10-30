db2b46d8d116e085e50d7ca45957bcaf4cfcc01d
chhaewxn <chaewon1019@ewhain.net>
2023-10-30 13:51:40 +0900

    refactor: PostJoin(게시글 참여신청) 구현 코드 리팩토링

---
+++ src/main/java/efub/back/jupjup/domain/postjoin/dto/PostjoinListResponseDto.java
@@ -0,0 +1,12 @@
+package efub.back.jupjup.domain.postjoin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostjoinListResponseDto {
+	private Long postId;
+	private String title;
+	// Post 관련 필드 추가하기
+}
