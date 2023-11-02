6c0943b3063e8aba5a69cda2f85036a9edd70d42
chhaewxn <chaewon1019@ewhain.net>
2023-11-03 05:29:23 +0900

    refactor: Postjoin(게시글 참여) 관련 기능 리팩토링

--- src/main/java/efub/back/jupjup/domain/postjoin/controller/PostjoinController.java
+++ src/main/java/efub/back/jupjup/domain/postjoin/controller/PostjoinController.java
@@ -1,57 +1,37 @@
 package efub.back.jupjup.domain.postjoin.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import efub.back.jupjup.domain.member.domain.Member;
-import efub.back.jupjup.domain.postjoin.dto.MemberProfileResponseDto;
-import efub.back.jupjup.domain.postjoin.dto.PostjoinListResponseDto;
-import efub.back.jupjup.domain.postjoin.dto.PostjoinResponseDto;
+import efub.back.jupjup.domain.post.domain.Post;
+import efub.back.jupjup.domain.post.repository.PostRepository;
 import efub.back.jupjup.domain.postjoin.service.PostjoinService;
 import efub.back.jupjup.domain.security.userInfo.AuthUser;
+import efub.back.jupjup.global.response.StatusResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/posts/{postId}/postjoins")
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostjoinController {
 	private final PostjoinService postjoinService;
+	private final PostRepository postRepository;
 
-	@PostMapping
-	public ResponseEntity<String> postJoin(@AuthUser Member member, @PathVariable("postId") Long postId) {
-		postjoinService.createByPostId(member, postId);
-		return new ResponseEntity<>(HttpStatus.OK);
+	// 게시글에 참여
+	@PostMapping("/{postId}/join")
+	public ResponseEntity<StatusResponse> joinPost(@PathVariable Long postId, @AuthUser Member member) {
+		Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다."));
+		return postjoinService.joinPost(member, postId);
 	}
 
-	@DeleteMapping
-	public ResponseEntity<String> deletePostjoin(@AuthUser Member member, @PathVariable("postId") Long postId) {
-		postjoinService.deleteByPostId(member, postId);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	@GetMapping
-	public ResponseEntity<PostjoinResponseDto> checkPostJoin(@AuthUser Member member, @PathVariable("postId") Long postId) {
-		PostjoinResponseDto postjoinResponseDto = postjoinService.findExistence(member, postId);
-		return ResponseEntity.status(HttpStatus.OK).body(postjoinResponseDto);
-	}
-
-	@GetMapping("/joined-members")
-	public ResponseEntity<List<MemberProfileResponseDto>> getJoinedMembers(@PathVariable Long postId) {
-		List<MemberProfileResponseDto> members = postjoinService.findAllMembersJoinedPost(postId);
-		return ResponseEntity.status(HttpStatus.OK).body(members);
-	}
-
-	@GetMapping("/joined")
-	public ResponseEntity<List<PostjoinListResponseDto>> getJoinedPosts(@AuthUser Member member) {
-		List<PostjoinListResponseDto> posts = postjoinService.findAllJoinedPostsByMember(member);
-		return ResponseEntity.status(HttpStatus.OK).body(posts);
+	// 게시글 참여 취소
+	@DeleteMapping("/{postId}/unjoin")
+	public ResponseEntity<StatusResponse> unjoinPost(@PathVariable Long postId, @AuthUser Member member) {
+		return postjoinService.unjoinPost(member, postId);
 	}
 }
