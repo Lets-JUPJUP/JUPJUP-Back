29b87f86a96695cd06e0fd2988e343b97deae3d0
chhaewxn <chaewon1019@ewhain.net>
2023-11-04 20:57:43 +0900

    fix: PostAgeRange 도메인 수정

--- src/main/java/efub/back/jupjup/domain/post/domain/PostAgeRange.java
+++ src/main/java/efub/back/jupjup/domain/post/domain/PostAgeRange.java
@@ -12,18 +12,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PostAgeRange {
 	AGE_0_9("0", "0~9"),
-	AGE_10_14("1", "10~14"),
-	AGE_15_19("2", "15~19"),
-	AGE_10_19("3", "10~19"),
-	AGE_20_29("4", "20~29"),
-	AGE_30_39("5", "30~39"),
-	AGE_40_49("6", "40~49"),
-	AGE_50_59("7", "50~59"),
-	AGE_60_69("8", "60~69"),
-	AGE_70_79("9", "70~79"),
-	AGE_80_80("10", "80~89"),
-	AGE_90_ABOVE("11", "90~"),
-	AGE_ANY("12", "any");
+	AGE_10_19("1", "10~19"),
+	AGE_20_29("2", "20~29"),
+	AGE_30_39("3", "30~39"),
+	AGE_40_49("4", "40~49"),
+	AGE_50_59("5", "50~59"),
+	AGE_60_69("6", "60~69"),
+	AGE_70_79("7", "70~79"),
+	AGE_80_80("8", "80~89"),
+	AGE_90_ABOVE("9", "90~"),
+	AGE_ANY("10", "any");
 
 	private final String code;
 	private final String description;
@@ -33,9 +31,6 @@ public enum PostAgeRange {
 
 	public static PostAgeRange fromString(final String ageRangeStr){
 		PostAgeRange ageRange = PostAgeRange.valueOf(AGE_RANGE_MAP.get(ageRangeStr));
-		if(ageRange.equals(PostAgeRange.AGE_10_14) || ageRange.equals(PostAgeRange.AGE_15_19)){
-			ageRange = PostAgeRange.AGE_10_19;
-		}
 
 		return ageRange;
 	}
