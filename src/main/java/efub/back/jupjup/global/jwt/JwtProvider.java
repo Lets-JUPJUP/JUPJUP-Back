28ac0983e86be84fd00edc4529178116e0f35428
chhaewxn <chaewon1019@ewhain.net>
2023-11-21 02:55:37 +0900

    feat: 토큰 만료 시간 1일로 수정

--- src/main/java/efub/back/jupjup/global/jwt/JwtProvider.java
+++ src/main/java/efub/back/jupjup/global/jwt/JwtProvider.java
@@ -31,7 +31,7 @@ public class JwtProvider {
     private String secretKey;
 
 //    private static final Long accessTokenValidationMs =  24*60 * 60 * 1000L; // 1일
-    private static final Long accessTokenValidationMs = 24 * 60 * 60 * 1000L; // 1일
+    private static final Long accessTokenValidationMs = 24 * 60 * 60 * 1000L;
     private static final Long refreshTokenValidationMs = 15 * 24 * 60 * 60 * 1000L; // 15일
     private static final String BEARER_PREFIX = "Bearer ";
 
