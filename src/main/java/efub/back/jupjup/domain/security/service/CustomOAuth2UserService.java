a25434f80d3fc50ef0110fb2940990ba097c60fb
Gayeong Park <maroony55@gmail.com>
2023-11-23 21:41:18 +0900

    hotfix : 카카오 gender null 처리 로직 수정

--- src/main/java/efub/back/jupjup/domain/security/service/CustomOAuth2UserService.java
+++ src/main/java/efub/back/jupjup/domain/security/service/CustomOAuth2UserService.java
@@ -1,95 +1,106 @@
 package efub.back.jupjup.domain.security.service;
 
-import efub.back.jupjup.domain.member.domain.*;
-import efub.back.jupjup.domain.member.repository.MemberRepository;
-import efub.back.jupjup.domain.security.userInfo.KakaoUserInfo;
-import efub.back.jupjup.domain.security.userInfo.PrincipalDetails;
-import efub.back.jupjup.domain.security.userInfo.OAuth2UserInfo;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import static efub.back.jupjup.domain.member.domain.Member.*;
+
+import java.util.Objects;
+
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import efub.back.jupjup.domain.member.domain.AgeRange;
+import efub.back.jupjup.domain.member.domain.Gender;
+import efub.back.jupjup.domain.member.domain.Member;
+import efub.back.jupjup.domain.member.domain.MemberStatus;
+import efub.back.jupjup.domain.member.domain.RoleType;
 import efub.back.jupjup.domain.member.exception.MemberNotFoundException;
-
-import java.util.Objects;
-
-import static efub.back.jupjup.domain.member.domain.Member.INFO_UNKNOWN;
-
+import efub.back.jupjup.domain.member.repository.MemberRepository;
+import efub.back.jupjup.domain.security.userInfo.KakaoUserInfo;
+import efub.back.jupjup.domain.security.userInfo.OAuth2UserInfo;
+import efub.back.jupjup.domain.security.userInfo.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.info("getAttributes : {}", oAuth2User.getAttributes());
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		log.info("getAttributes : {}", oAuth2User.getAttributes());
 
-        OAuth2UserInfo oAuth2UserInfo = null;
-        String provider = userRequest.getClientRegistration().getRegistrationId();
+		OAuth2UserInfo oAuth2UserInfo = null;
+		String provider = userRequest.getClientRegistration().getRegistrationId();
 
-        if(provider.equals("kakao")){
-            log.info("카카오 로그인");
-            oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes());
-        }
+		if (provider.equals("kakao")) {
+			log.info("카카오 로그인");
+			oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes());
+		}
 
-        Member member = saveOrUpdate(oAuth2UserInfo);
-        return new PrincipalDetails(member, oAuth2User.getAttributes());
-    }
+		Member member = saveOrUpdate(oAuth2UserInfo);
+		return new PrincipalDetails(member, oAuth2User.getAttributes());
+	}
 
-    private Member saveOrUpdate(OAuth2UserInfo oAuth2UserInfo) {
-        Member member = memberRepository.findByUsername(oAuth2UserInfo.getName())
-                .map(entity -> {
-                    entity.updateMember(oAuth2UserInfo.getEmail(), oAuth2UserInfo.getAgeRange());
-                    return entity;
-                })
-                .orElseGet(() -> {
-                    if (!memberRepository.existsByEmail(oAuth2UserInfo.getEmail())) {
-                        String nickname = oAuth2UserInfo.getNickname();
-                        log.info("카카오 닉네임 : " + nickname);
-                        if(invalidateNickname(nickname)){
-                            nickname = String.valueOf(System.currentTimeMillis());
-                        }
-                        return Member.builder()
-                                .nickname(nickname)
-                                .email(oAuth2UserInfo.getEmail())
-                                .profileImageUrl(oAuth2UserInfo.getProfileImageUrl())
-                                .username(oAuth2UserInfo.getName())
-                                .providerType(oAuth2UserInfo.getProvider())
-                                .ageRange(AgeRange.fromString(oAuth2UserInfo.getAgeRange()))
-                                .gender(oAuth2UserInfo.getGender() == null ? Gender.NOT_DEFINED : Gender.valueOf(oAuth2UserInfo.getGender().toUpperCase().trim()))
-                                .roleType(RoleType.MEMBER)
-                                .status(MemberStatus.ACTIVE)
-                                .build();
-                    } else {
-                        return null;
-                    }
-                });
+	private Member saveOrUpdate(OAuth2UserInfo oAuth2UserInfo) {
+		Member member = memberRepository.findByUsername(oAuth2UserInfo.getName())
+			.map(entity -> {
+				entity.updateMember(oAuth2UserInfo.getEmail(), oAuth2UserInfo.getAgeRange());
+				return entity;
+			})
+			.orElseGet(() -> {
+				if (!memberRepository.existsByEmail(oAuth2UserInfo.getEmail())) {
+					String nickname = oAuth2UserInfo.getNickname();
+					log.info("카카오 닉네임 : " + nickname);
+					if (invalidateNickname(nickname)) {
+						nickname = String.valueOf(System.currentTimeMillis());
+					}
+					Gender gender = null;
+					if (oAuth2UserInfo.getGender() == null) {
+						gender = Gender.NOT_DEFINED;
+					} else {
+						gender = Gender.valueOf(oAuth2UserInfo.getGender().toUpperCase().trim());
+					}
+					return Member.builder()
+						.nickname(nickname)
+						.email(oAuth2UserInfo.getEmail())
+						.profileImageUrl(oAuth2UserInfo.getProfileImageUrl())
+						.username(oAuth2UserInfo.getName())
+						.providerType(oAuth2UserInfo.getProvider())
+						.ageRange(AgeRange.fromString(oAuth2UserInfo.getAgeRange()))
+						.gender(gender)
+						.roleType(RoleType.MEMBER)
+						.status(MemberStatus.ACTIVE)
+						.build();
+				} else {
+					return null;
+				}
+			});
 
-        if (member == null) {
-            member = memberRepository.findByEmail(oAuth2UserInfo.getEmail()).orElseThrow(MemberNotFoundException::new);
-            member.updateMember(oAuth2UserInfo.getEmail(), oAuth2UserInfo.getAgeRange());
-            return member;
-        }
+		if (member == null) {
+			member = memberRepository.findByEmail(oAuth2UserInfo.getEmail()).orElseThrow(MemberNotFoundException::new);
+			member.updateMember(oAuth2UserInfo.getEmail(), oAuth2UserInfo.getAgeRange());
+			return member;
+		}
 
-        return memberRepository.save(member);
-    }
+		return memberRepository.save(member);
+	}
 
-    public boolean invalidateNickname(String nickname) {
-        if (Objects.isNull(nickname) || nickname.isBlank() || nickname.length() > 15 || !nickname.matches("[ㄱ-ㅎ가-힣a-zA-Z0-9_]+") || nickname.equalsIgnoreCase(INFO_UNKNOWN)) {
-            return true;
-        }
+	public boolean invalidateNickname(String nickname) {
+		if (Objects.isNull(nickname) || nickname.isBlank() || nickname.length() > 15 || !nickname.matches(
+			"[ㄱ-ㅎ가-힣a-zA-Z0-9_]+") || nickname.equalsIgnoreCase(INFO_UNKNOWN)) {
+			return true;
+		}
 
-        if (memberRepository.existsByNickname(nickname)) {
-            return true;
-        }
-        return false;
-    }
+		if (memberRepository.existsByNickname(nickname)) {
+			return true;
+		}
+		return false;
+	}
 }
