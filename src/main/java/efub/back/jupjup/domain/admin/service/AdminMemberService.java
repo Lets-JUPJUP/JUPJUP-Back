d83851f3dc7d53917ad52b3a9b939bf70cfd242f
Gayeong Park <maroony55@gmail.com>
2024-01-25 22:11:52 +0900

    feat : 관리자 회원 탈퇴시 redis 리프레시 토큰 삭제
    Related to : #66

--- src/main/java/efub/back/jupjup/domain/admin/service/AdminMemberService.java
+++ src/main/java/efub/back/jupjup/domain/admin/service/AdminMemberService.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import efub.back.jupjup.domain.auth.service.AuthService;
 import efub.back.jupjup.domain.member.domain.Member;
 import efub.back.jupjup.domain.member.domain.MemberStatus;
 import efub.back.jupjup.domain.member.exception.MemberNotFoundException;
@@ -21,10 +22,12 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminMemberService {
 
 	private final MemberRepository memberRepository;
+	private final AuthService authService;
 
 	public ResponseEntity<StatusResponse> blockMember(Long memberId) {
 		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
 		member.updateMemberStatus(MemberStatus.BLOCKED);
+		authService.blockInRedis(member.getEmail());
 		return ResponseEntity.ok(StatusResponse.builder()
 			.status(StatusEnum.OK.getStatusCode())
 			.message(StatusEnum.OK.getCode())
