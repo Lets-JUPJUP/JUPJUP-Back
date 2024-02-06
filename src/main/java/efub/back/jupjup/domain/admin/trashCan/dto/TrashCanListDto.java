8ac961340bcb4595cc666dc43a19fa229f42e62e
Gayeong Park <maroony55@gmail.com>
2024-02-06 22:14:27 +0900

    feat : 관리자 쓰레기통 조회 페이징 적용
    Related to : #65

--- src/main/java/efub/back/jupjup/domain/admin/trashCan/dto/TrashCanListDto.java
+++ src/main/java/efub/back/jupjup/domain/admin/trashCan/dto/TrashCanListDto.java
@@ -2,12 +2,13 @@ package efub.back.jupjup.domain.admin.trashCan.dto;
 
 import java.util.List;
 
+import efub.back.jupjup.global.response.PageInfo;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class TrashCanListDto {
-	private int trashCanCount;
+	private PageInfo pageInfo;
 	private List<TrashCanSimpleDto> trashCans;
 }
