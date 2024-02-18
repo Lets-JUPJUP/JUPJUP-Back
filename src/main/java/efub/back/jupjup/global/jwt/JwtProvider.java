eca66228800ea3e5e8468287b06be92584b41d07
Gayeong Park <maroony55@gmail.com>
2024-02-18 22:18:24 +0900

    hotfix : 테스트용 토큰 시간 임시 변경

--- src/main/java/efub/back/jupjup/global/jwt/JwtProvider.java
+++ src/main/java/efub/back/jupjup/global/jwt/JwtProvider.java
@@ -1,196 +1,206 @@
 package efub.back.jupjup.global.jwt;
 
-import efub.back.jupjup.domain.security.exception.*;
-import efub.back.jupjup.domain.security.service.UserDetailsServcieImpl;
-import efub.back.jupjup.global.exception.custom.UnAuthorizedException;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Date;
+import efub.back.jupjup.domain.security.exception.AuthExceptionHandler;
+import efub.back.jupjup.domain.security.exception.EmptyTokenException;
+import efub.back.jupjup.domain.security.exception.ExpiredTokenException;
+import efub.back.jupjup.domain.security.exception.InvalidTokenException;
+import efub.back.jupjup.domain.security.exception.InvalidTokenTypeException;
+import efub.back.jupjup.domain.security.service.UserDetailsServcieImpl;
+import efub.back.jupjup.global.exception.custom.UnAuthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtProvider {
-    private final AuthExceptionHandler authExceptionHandler;
+	private final AuthExceptionHandler authExceptionHandler;
 
-    private final UserDetailsServcieImpl userDetailsService;
+	private final UserDetailsServcieImpl userDetailsService;
 
-    @Value("${spring.jwt.secret-key}")
-    private String secretKey;
+	@Value("${spring.jwt.secret-key}")
+	private String secretKey;
 
-//    private static final Long accessTokenValidationMs =  24*60 * 60 * 1000L; // 1일
-    private static final Long accessTokenValidationMs = 24 * 60 * 60 * 1000L;
-    private static final Long refreshTokenValidationMs = 15 * 24 * 60 * 60 * 1000L; // 15일
-    private static final String BEARER_PREFIX = "Bearer ";
+	//    private static final Long accessTokenValidationMs =  24 * 60 * 60 * 1000L; // 1일
+	// private static final Long refreshTokenValidationMs = 15 * 24 * 60 * 60 * 1000L; // 15일
+	private static final Long accessTokenValidationMs = 60 * 1000L;
+	private static final Long refreshTokenValidationMs = 5 * 60 * 1000L;
+	private static final String BEARER_PREFIX = "Bearer ";
 
-    public Long getRefreshTokenValidationMs() { // Redis에 저장 시 사용
-        return refreshTokenValidationMs;
-    }
+	public Long getRefreshTokenValidationMs() { // Redis에 저장 시 사용
+		return refreshTokenValidationMs;
+	}
 
-    // AccessToken 생성
-    public String generateAccessToken(String email) {
+	// AccessToken 생성
+	public String generateAccessToken(String email) {
 
-        // Registered claim. 토큰에 대한 정보들이 담겨있는 클레임. 이미 이름이 등록되어있다.
-        Claims claims = Jwts.claims()
-                // 토큰 제목(sub). 고유 식별자를 넣는다.
-                .setSubject(email)
-                // 발급 시간(iat)
-                .setIssuedAt(new Date())
-                // 만료 시간(exp)
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidationMs));
+		// Registered claim. 토큰에 대한 정보들이 담겨있는 클레임. 이미 이름이 등록되어있다.
+		Claims claims = Jwts.claims()
+			// 토큰 제목(sub). 고유 식별자를 넣는다.
+			.setSubject(email)
+			// 발급 시간(iat)
+			.setIssuedAt(new Date())
+			// 만료 시간(exp)
+			.setExpiration(new Date(System.currentTimeMillis() + accessTokenValidationMs));
 
+		return Jwts.builder()
+			// 헤더의 타입(typ)을 jwt로 설정
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			// 생성된 Claims 등록
+			.setClaims(claims)
+			// 서명에 사용할 키와 해싱 알고리즘(alt) 설정
+			.signWith(getSignKey(secretKey), SignatureAlgorithm.HS256)
+			// JWT 생성
+			.compact();
+	}
 
-        return Jwts.builder()
-                // 헤더의 타입(typ)을 jwt로 설정
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                // 생성된 Claims 등록
-                .setClaims(claims)
-                // 서명에 사용할 키와 해싱 알고리즘(alt) 설정
-                .signWith(getSignKey(secretKey), SignatureAlgorithm.HS256)
-                // JWT 생성
-                .compact();
-    }
+	// RefreshToken 생성
+	public String generateRefreshToken(String email) {
 
-    // RefreshToken 생성
-    public String generateRefreshToken(String email) {
+		Claims claims = Jwts.claims()
+			.setSubject(email)
+			.setIssuedAt(new Date())
+			.setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidationMs));
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			.setClaims(claims)
+			.signWith(getSignKey(secretKey), SignatureAlgorithm.HS256)
+			.compact();
+	}
 
-        Claims claims = Jwts.claims()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidationMs));
-        return Jwts.builder()
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setClaims(claims)
-                .signWith(getSignKey(secretKey), SignatureAlgorithm.HS256)
-                .compact();
-    }
+	// String인 secretKey를 byte[]로 변환 후 반환한다.
+	private Key getSignKey(String secretKey) {
+		return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+	}
 
-    // String인 secretKey를 byte[]로 변환 후 반환한다.
-    private Key getSignKey(String secretKey) {
-        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-    }
+	public boolean validateToken(HttpServletResponse response, String token) throws
+		IOException {
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(getSignKey(secretKey))
+				.build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (ExpiredJwtException e) {
+			log.error("만료된 토큰입니다. {}", e.toString());
+			authExceptionHandler.handleException(response, new ExpiredTokenException());
+			return false;
+		} catch (UnsupportedJwtException e) {
+			log.error("잘못된 형식의 토큰입니다. {}", e.toString());
+			authExceptionHandler.handleException(response, new InvalidTokenTypeException());
+			return false;
+		} catch (MalformedJwtException e) {
+			log.error("잘못된 구조의 토큰입니다. {}", e.toString());
+			authExceptionHandler.handleException(response, new InvalidTokenTypeException());
+			return false;
+		} catch (IllegalArgumentException e) {
+			log.error("잘못 생성된 토큰입니다. {}", e.toString());
+			authExceptionHandler.handleException(response, new InvalidTokenException());
+			return false;
+		}
+	}
 
-    public boolean validateToken(HttpServletResponse response, String token) throws
-            IOException {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSignKey(secretKey))
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.error("만료된 토큰입니다. {}", e.toString());
-            authExceptionHandler.handleException(response, new ExpiredTokenException());
-            return false;
-        } catch (UnsupportedJwtException e) {
-            log.error("잘못된 형식의 토큰입니다. {}", e.toString());
-            authExceptionHandler.handleException(response, new InvalidTokenTypeException());
-            return false;
-        } catch (MalformedJwtException e) {
-            log.error("잘못된 구조의 토큰입니다. {}", e.toString());
-            authExceptionHandler.handleException(response, new InvalidTokenTypeException());
-            return false;
-        }
-        catch (IllegalArgumentException e) {
-            log.error("잘못 생성된 토큰입니다. {}", e.toString());
-            authExceptionHandler.handleException(response, new InvalidTokenException());
-            return false;
-        }
-    }
-    public boolean validateToken(String token)
-    {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSignKey(secretKey))
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.error("만료된 토큰입니다. {}", e.toString());
-            return false;
-        } catch (UnsupportedJwtException e) {
-            log.error("잘못된 형식의 토큰입니다. {}", e.toString());
-            return false;
-        } catch (MalformedJwtException e) {
-            log.error("잘못된 구조의 토큰입니다. {}", e.toString());
-            return false;
-        }
-        catch (IllegalArgumentException e) {
-            log.error("잘못 생성된 토큰입니다. {}", e.toString());
-            return false;
-        }
-    }
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(getSignKey(secretKey))
+				.build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (ExpiredJwtException e) {
+			log.error("만료된 토큰입니다. {}", e.toString());
+			return false;
+		} catch (UnsupportedJwtException e) {
+			log.error("잘못된 형식의 토큰입니다. {}", e.toString());
+			return false;
+		} catch (MalformedJwtException e) {
+			log.error("잘못된 구조의 토큰입니다. {}", e.toString());
+			return false;
+		} catch (IllegalArgumentException e) {
+			log.error("잘못 생성된 토큰입니다. {}", e.toString());
+			return false;
+		}
+	}
 
-    // JWT payload를 복호화해서 반환
-    public Claims getClaims(String token) {
-        try {
-            return Jwts.parserBuilder() // JwtParserBuilder 인스턴스 생성
-                    .setSigningKey(getSignKey(secretKey)) // JWT Signature 검증을 위한 SecretKey 설정
-                    .build() // Thread-Safe한 JwtParser를 반환하기 위해 build 호출
-                    .parseClaimsJws(token) // Claim(Payload) 파싱
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            // 만료된 토큰이어도 refresh token 검증 후 재발급할 수 있또록 claims 반환
-            return e.getClaims();
-        } catch (Exception e) {
-            // 다른 예외인 경우 throw
-            log.error("유효하지 않은 토큰입니다. {}", e.toString());
-            throw new InvalidTokenException();
-        }
-    }
+	// JWT payload를 복호화해서 반환
+	public Claims getClaims(String token) {
+		try {
+			return Jwts.parserBuilder() // JwtParserBuilder 인스턴스 생성
+				.setSigningKey(getSignKey(secretKey)) // JWT Signature 검증을 위한 SecretKey 설정
+				.build() // Thread-Safe한 JwtParser를 반환하기 위해 build 호출
+				.parseClaimsJws(token) // Claim(Payload) 파싱
+				.getBody();
+		} catch (ExpiredJwtException e) {
+			// 만료된 토큰이어도 refresh token 검증 후 재발급할 수 있또록 claims 반환
+			return e.getClaims();
+		} catch (Exception e) {
+			// 다른 예외인 경우 throw
+			log.error("유효하지 않은 토큰입니다. {}", e.toString());
+			throw new InvalidTokenException();
+		}
+	}
 
-    public Authentication getAuthentication(String token) {
+	public Authentication getAuthentication(String token) {
 
-        Claims claims = getClaims(token);
-        String email = claims.getSubject();
+		Claims claims = getClaims(token);
+		String email = claims.getSubject();
 
-        if (email == null) {
-            log.error("권한 정보가 없는 토큰입니다. {}", token);
-            throw new UnAuthorizedException();
-        }
+		if (email == null) {
+			log.error("권한 정보가 없는 토큰입니다. {}", token);
+			throw new UnAuthorizedException();
+		}
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
+		UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
 
-    public String tokenToEmail(String authorization){
-        String accessToken = getAccessToken(authorization);
-        Claims claims = getClaims(accessToken);
-        return claims.getSubject();
-    }
-    public String getAccessToken(String authorizationHeader) {
+	public String tokenToEmail(String authorization) {
+		String accessToken = getAccessToken(authorization);
+		Claims claims = getClaims(accessToken);
+		return claims.getSubject();
+	}
 
-        if(authorizationHeader == null || authorizationHeader.equals("null")){
-            throw new EmptyTokenException();
-        }
-        String token = authorizationHeader;
-        if(authorizationHeader.startsWith(BEARER_PREFIX)){
-            token = authorizationHeader.substring(BEARER_PREFIX.length());
-        }
-        else if(authorizationHeader.startsWith("[Bearer ")){
-            token = authorizationHeader.substring(BEARER_PREFIX.length()+1);
-        }
-        if (token.endsWith("]")) {
-            token = token.substring(0, token.length() - 1);
-        }
-        return token;
-    }
+	public String getAccessToken(String authorizationHeader) {
 
-    public Long getRemainingTime(String token) {
-        Date expiration = getClaims(token).getExpiration();
-        Date now = new Date();
-        return expiration.getTime() - now.getTime();
-    }
+		if (authorizationHeader == null || authorizationHeader.equals("null")) {
+			throw new EmptyTokenException();
+		}
+		String token = authorizationHeader;
+		if (authorizationHeader.startsWith(BEARER_PREFIX)) {
+			token = authorizationHeader.substring(BEARER_PREFIX.length());
+		} else if (authorizationHeader.startsWith("[Bearer ")) {
+			token = authorizationHeader.substring(BEARER_PREFIX.length() + 1);
+		}
+		if (token.endsWith("]")) {
+			token = token.substring(0, token.length() - 1);
+		}
+		return token;
+	}
+
+	public Long getRemainingTime(String token) {
+		Date expiration = getClaims(token).getExpiration();
+		Date now = new Date();
+		return expiration.getTime() - now.getTime();
+	}
 }
