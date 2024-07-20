e216a05ea824d6a3eaf9e1dbbde31e41597cde4e
Gayeong Park <maroony55@gmail.com>
2024-07-20 15:16:22 +0900

    refactor : Member에 의존하는 로직 수정

--- src/main/java/efub/back/jupjup/domain/member/dto/response/MemberResDto.java
+++ src/main/java/efub/back/jupjup/domain/member/dto/response/MemberResDto.java
@@ -1,6 +1,5 @@
 package efub.back.jupjup.domain.member.dto.response;
 
-import efub.back.jupjup.domain.member.domain.AgeRange;
 import efub.back.jupjup.domain.member.domain.Gender;
 import efub.back.jupjup.domain.member.domain.Member;
 import efub.back.jupjup.domain.security.userInfo.ProviderType;
@@ -10,23 +9,23 @@ import lombok.Getter;
 @Getter
 @Builder
 public class MemberResDto {
-    private Long id;
-    private String email;
-    private String profileImageUrl;
-    private String nickname;
-    private ProviderType providerType;
-    private AgeRange ageRange;
-    private Gender gender;
+	private Long id;
+	private String email;
+	private String profileImageUrl;
+	private String nickname;
+	private ProviderType providerType;
+	private Integer age;
+	private Gender gender;
 
-    public static MemberResDto from(Member member){
-        return MemberResDto.builder()
-                .id(member.getId())
-                .email(member.getEmail())
-                .profileImageUrl(member.getProfileImageUrl())
-                .nickname(member.getNickname())
-                .providerType(member.getProviderType())
-                .ageRange(member.getAgeRange())
-                .gender(member.getGender())
-                .build();
-    }
+	public static MemberResDto from(Member member) {
+		return MemberResDto.builder()
+			.id(member.getId())
+			.email(member.getEmail())
+			.profileImageUrl(member.getProfileImageUrl())
+			.nickname(member.getNickname())
+			.providerType(member.getProviderType())
+			.age(member.getAge())
+			.gender(member.getGender())
+			.build();
+	}
 }
--- src/main/java/efub/back/jupjup/domain/member/dto/response/MyProfileResDto.java
+++ src/main/java/efub/back/jupjup/domain/member/dto/response/MyProfileResDto.java
@@ -1,6 +1,5 @@
 package efub.back.jupjup.domain.member.dto.response;
 
-import efub.back.jupjup.domain.member.domain.AgeRange;
 import efub.back.jupjup.domain.member.domain.Gender;
 import efub.back.jupjup.domain.member.domain.Member;
 import efub.back.jupjup.domain.security.userInfo.ProviderType;
@@ -10,25 +9,25 @@ import lombok.Getter;
 @Getter
 @Builder
 public class MyProfileResDto {
-    private Long id;
-    private String email;
-    private String profileImageUrl;
-    private String nickname;
-    private ProviderType providerType;
-    private AgeRange ageRange;
-    private Gender gender;
-    private Boolean isProfileCreated;
+	private Long id;
+	private String email;
+	private String profileImageUrl;
+	private String nickname;
+	private ProviderType providerType;
+	private Integer age;
+	private Gender gender;
+	private Boolean isProfileCreated;
 
-    public static MyProfileResDto from(Member member){
-        return MyProfileResDto.builder()
-                .id(member.getId())
-                .email(member.getEmail())
-                .profileImageUrl(member.getProfileImageUrl())
-                .nickname(member.getNickname())
-                .providerType(member.getProviderType())
-                .ageRange(member.getAgeRange())
-                .gender(member.getGender())
-                .isProfileCreated(member.getModifiedAt() != null)
-                .build();
-    }
+	public static MyProfileResDto from(Member member) {
+		return MyProfileResDto.builder()
+			.id(member.getId())
+			.email(member.getEmail())
+			.profileImageUrl(member.getProfileImageUrl())
+			.nickname(member.getNickname())
+			.providerType(member.getProviderType())
+			.age(member.getAge())
+			.gender(member.getGender())
+			.isProfileCreated(member.getModifiedAt() != null)
+			.build();
+	}
 }
