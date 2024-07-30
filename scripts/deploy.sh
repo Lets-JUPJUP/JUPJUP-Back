186cd14d4329b4be844909e7ac0cf59c62119806
Gayeong Park <maroony55@gmail.com>
2024-07-31 00:51:52 +0900

    fix : update deploy.sh
    changed blue-green deployment logic again..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,62 +1,26 @@
 #!/bin/bash
 
-IS_DEV1=$(docker ps | grep jupjup-blue)
-DEFAULT_CONF=" /etc/nginx/nginx.conf"
-MAX_RETRIES=20
+cd /home/ubuntu/app
 
-check_service() {
-  local RETRIES=0
-  local URL=$1
-  while [ $RETRIES -lt $MAX_RETRIES ]; do
-    echo "Checking service at $URL... (attempt: $((RETRIES+1)))"
-    sleep 3
+DOCKER_APP_NAME=spring
 
-    REQUEST=$(curl $URL)
-    if [ -n "$REQUEST" ]; then
-      echo "health check success"
-      return 0
-    fi
+EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+echo "$EXIST_BLUE" >> debug.log
+if [ -z "$EXIST_BLUE" ]; then
+	echo "blue up" >> debug.log
+	docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
-    RETRIES=$((RETRIES+1))
-  done;
+	sleep 30
 
-  echo "Failed to check service after $MAX_RETRIES attempts."
-  return 1
-}
-
-if [ -z "$IS_DEV1" ];then
-  echo "## DEV2 => DEV1"
-  docker-compose up -d dev1
-
-  echo "## health check"
-  if ! check_service "http://127.0.0.1:8081"; then
-    echo "DEV1 health check 가 실패했습니다."
-    exit 1
-  fi
-
-  echo "## nginx 재실행"
-  sudo cp /etc/nginx/nginx.dev1.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
-  echo "## DEV2 컨테이너 중지 및 제거"
-  docker-compose stop dev2
-  docker-compose rm -f dev2
+	docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	docker image prune -af
 
 else
-  echo "## DEV1 => DEV2"
-  docker-compose up -d dev2
+	echo "green up" >> debug.log
+	docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
-  echo "## health check"
-  if ! check_service "http://127.0.0.1:8082"; then
-      echo "DEV1 health check 가 실패했습니다."
-      exit 1
-    fi
+	sleep 30
 
-  echo "## nginx 재실행"
-  sudo cp /etc/nginx/nginx.dev2.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
-  echo "## DEV1 컨테이너 중지 및 제거"
-  docker-compose stop dev1
-  docker-compose rm -f dev2
+	docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	docker image prune -af
 fi
