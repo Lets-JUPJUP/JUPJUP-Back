2c7e226515006540b30ae55ec08defe5d5fe90e8
Gayeong Park <maroony55@gmail.com>
2024-07-30 23:35:09 +0900

    fix : update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,20 @@
 cd /home/ubuntu/app
 DOCKER_APP_NAME=jupjup
 # blue를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 컨테이너 스위칭
 # blue가 없으면
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker compose -p ${DOCKER_APP_NAME}-blue -f docker compose.blue.yml up --build -d
+    docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up --build -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 
 # blue가 있으면
 else
     echo "green up"
-    docker compose -p ${DOCKER_APP_NAME}-green -f docker compose.green.yml up --build -d
+    docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up --build -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
@@ -26,13 +26,13 @@ sleep 10
 echo "End sleep"
 
 # 새로운 컨테이너가 제대로 떴있는지 확인
-EXIST_AFTER=$(docker compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
+EXIST_AFTER=$(docker compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 
 # {EXIST_AFTER}가 있으면
 if [ -n "$EXIST_AFTER" ]; then
   # 이전 컨테이너 종료
   echo "$BEFORE_COMPOSE_COLOR down"
-  docker compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker compose.${BEFORE_COMPOSE_COLOR}.yml down
+  docker compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
 else
     echo "> The new container did not run properly."
     exit 1  
