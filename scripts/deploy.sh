0a27bd0e8f7da4ac86f9347276060f7507190323
Gayeong Park <maroony55@gmail.com>
2024-07-31 01:00:43 +0900

    fix :  update deploy.sh
    docker-compose -> docker compose -> docker-compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,25 +2,25 @@
 
 cd /home/ubuntu/app
 
-DOCKER_APP_NAME=spring
+DOCKER_APP_NAME=server
 
-EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 echo "$EXIST_BLUE" >> debug.log
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up" >> debug.log
-	docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
 	sleep 30
 
-	docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 	docker image prune -af
 
 else
 	echo "green up" >> debug.log
-	docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
 	sleep 30
 
-	docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 	docker image prune -af
 fi
